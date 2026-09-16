eaa3a846cd734bfbf3acefc27a5da280c60468ba
kshitizsengar089-ship-it <kshitizsengar089@gmail.com>
2026-09-17 03:34:42 +0530

    Time: 0 ms (100%), Space: 47.7 MB (53.48%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int trap(int[] height) {
+    
+    if (height==null || height.length==0)
+    {
+        return 0;
+    }
+    int left=0;
+    int right=height.length-1;
+    int leftmax=0;
+    int rightmax=0;
+    int water=0;
+
+    while(left<right)
+    {
+        if(height[left]<=height[right]){
+            if(height[left]>=leftmax){
+                leftmax=height[left];
+            }
+            else{
+                water+=leftmax-height[left];
+            }
+            left++;
+        }else{
+           if(height[right]>=rightmax){
+               rightmax=height[right];
+           }else{
+            water+=rightmax-height[right];
+           }
+           right--;
+        }
+    }
+    return water;
+    
+     }
+}
